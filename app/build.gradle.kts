6e8933235a7af44f57f788fdb75909e8a9a487e9
Andrey Sidorov <cugopob.a@yandex.ru>
2025-12-15 01:19:42 +0700

    Removed Android Material library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,15 +43,14 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.navigation.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.material.icons.core)
-    implementation(libs.material)
 
     implementation(libs.okhttp)
     implementation(libs.kotlinx.serialization.json)
